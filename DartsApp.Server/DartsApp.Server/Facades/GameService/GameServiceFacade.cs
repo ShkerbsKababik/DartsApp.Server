8b62a9ff9c50ea0317e7d6c3516a0c7ea44be40f
Vladimir <110543788+ShkerbsKababik@users.noreply.github.com>
2025-04-14 22:46:09 +0300

    new game facade

--- DartsApp.Server/DartsApp.Server/Facades/GameService/GameServiceFacade.cs
+++ DartsApp.Server/DartsApp.Server/Facades/GameService/GameServiceFacade.cs
@@ -1,4 +1,6 @@
 ﻿
+using Microsoft.AspNetCore.Http.HttpResults;
+
 namespace DartsApp.Server.Facades
 {
     public class GameServiceFacade : IGameServiceFacade
@@ -10,43 +12,59 @@ namespace DartsApp.Server.Facades
         }
 
         public Guid CreateGame(GameCreationInfo gameCreationInfo)
+            => CreateGameAsync(gameCreationInfo).Result;
+
+        public async Task<Guid> CreateGameAsync(GameCreationInfo gameCreationInfo)
         {
-            var owner = _dartsDbContext.Users.Where(u => u.Id == gameCreationInfo.OwnerId).FirstOrDefault();
-            if (owner != null)
+            var owner = _dartsDbContext.Users.FirstOrDefault(u => u.Id == gameCreationInfo.OwnerId);
+            var players = _dartsDbContext.Users.Where(u => gameCreationInfo.PlayerIds.Contains(u.Id));
+            if (owner == null && players.Count() != gameCreationInfo.PlayerIds.Count) throw new Exception("invalid data");
+
+            var game = new Game()
             {
-                var game = new Game()
+                Id = Guid.NewGuid(),
+                Owner = owner,
+                CurrentPlayer = owner,
+                Players = new List<User>(),
+                Scores = new List<Score>()
+            };
+
+            foreach (var player in players)
+            {
+                game.Players.Add(player);
+                game.Scores.Add(new Score() 
                 {
-                    Owner = owner,
-                    CurrentPlayer = owner,
-                };
-
-                foreach (var playerId in gameCreationInfo.PlayerIds)
-                {
-                    var player = _dartsDbContext.Users.Where(u => u.Id == playerId).FirstOrDefault();
-                    if (player != null)
-                    { 
-                        game.Players.Add(player);
-                        game.Scores.Add(new Score() 
-                        { 
-                            Game = game,
-                            Owner = player,
-                            Value = 301
-                        });
-                    }
-                }
-
-
+                    Id = Guid.NewGuid(),
+                    Game = game,
+                    Owner = player,
+                    Value = 301
+                });
             }
+
+            await _dartsDbContext.Games.AddAsync(game);
+            await _dartsDbContext.SaveChangesAsync();
+
+            return game.Id;
         }
 
         public GameInfo GetGameInfo(Guid gameId)
         {
-            throw new NotImplementedException();
+            var game = _dartsDbContext.Games.FirstOrDefault(g => g.Id == gameId);
+            if (game == null) throw new Exception("invalid data");
+
+            return GameInfo.FromDomain(game);
         }
 
         public void UpdateScore(GameScoreInfo gameScoreInfo)
+            => UpdateScoreAsync(gameScoreInfo).Wait();
+
+        public async Task UpdateScoreAsync(GameScoreInfo gameScoreInfo)
         {
-            throw new NotImplementedException();
+            var score = _dartsDbContext.Scores.FirstOrDefault(s => s.Id == gameScoreInfo.ScoreId);
+            if (score == null) throw new Exception("invalid data");
+
+            score.Value = gameScoreInfo.Value;
+            await _dartsDbContext.SaveChangesAsync();
         }
     }
 }
--- DartsApp.Server/DartsApp.Server/Facades/GameService/IGameServiceFacade.cs
+++ DartsApp.Server/DartsApp.Server/Facades/GameService/IGameServiceFacade.cs
@@ -35,7 +35,8 @@
     }
 
     public class GameScoreInfo
-    { 
-           
+    {
+        public Guid ScoreId { get; set; }
+        public int Value { get; set; }
     }
 }
