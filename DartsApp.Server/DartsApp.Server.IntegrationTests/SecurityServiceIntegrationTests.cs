3074dfe65c02ffc73680f40c80c28e4a12d075d1
Vladimir <110543788+ShkerbsKababik@users.noreply.github.com>
2025-05-27 11:05:30 +0300

    kaifik 2

--- DartsApp.Server/DartsApp.Server.IntegrationTests/SecurityServiceIntegrationTests.cs
+++ DartsApp.Server/DartsApp.Server.IntegrationTests/SecurityServiceIntegrationTests.cs
@@ -2,6 +2,7 @@
 using System.Net.Http.Json;
 using System.Net;
 using DartsApp.Server.Client;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 
 namespace DartsApp.Server.IntegrationTests
 {
@@ -17,13 +18,38 @@ namespace DartsApp.Server.IntegrationTests
         }
 
         [Fact]
-        public async Task CheckAnonymousAccessDenied()
+        public async Task AnonymousAccessTest()
         {
             // Act
             var responce = await _client.CheckAnonymousAsync();
 
             // Assert
             Assert.NotNull(responce);
+            Assert.Equal("allowed anonymous", responce);
+        }
+
+        [Fact]
+        public async Task NonAuthorizeAccessTest()
+        {
+            // Act & Assert
+            var exception = await Assert.ThrowsAsync<ApiException>(_client.CheckAuthorizeAsync);
+            Assert.Contains("405", exception.Message);
+        }
+
+        [Fact]
+        public async Task AuthorizeAccessTest()
+        {
+            // Act
+            await _client.LoginAsync(new Client.AuthenticationInfo()
+            {
+                Login = "admin",
+                Password = "admin"
+            });
+            var responce = await _client.CheckAuthorizeAsync();
+
+            // Assert
+            Assert.NotNull(responce);
+            Assert.Equal("allowed authorize", responce);
         }
     }
 }
